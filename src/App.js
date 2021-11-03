1587ac58d427cc0c2dbaba61106e696fafcb0df1
beanxx <tnqls991211@naver.com>
2021-11-03 14:28:05 +0900

    feat: Login(home) page css fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
-import back_ground from './image/party.jpg'
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./containers/Login";
+import SignupPage from "./SignupPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={back_ground} resizeMode="cover" alt="profile"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignupPage} />
     </div>
   );
 }
