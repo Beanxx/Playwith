83e6c61753fe74283a52daa58b8e6f3c2a1ab2eb
이혜인 <leehi9817@gmail.com>
2021-11-03 13:28:29 +0900

    SignupPage.js

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,167 +1,27 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import back_ground from "./image/birthday_cake.jpg";
+import styled from "styled-components";
 
+<img src={back_ground} resizeMode="cover" alt="profile" />;
 function SignupPage() {
-  const [Id, setId] = useState("");
-  const [Password, setPassword] = useState("");
-  const [ConfirmPassword, setConfirmPassword] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Name, setName] = useState("");
-
-  const onIdHandler = (event) => {
-    setId(event.currentTarget.value);
-  };
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  };
-
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.currentTarget.value);
-  };
-
-  const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.value);
-  };
-
-  const onNameHandler = (event) => {
-    setName(event.currentTarget.value);
-  };
-
-  return (
-    <div
-      style={{
-        disply: "flex",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <Container>
-        <div
-          style={{
-            display: "flex",
-            float: "right",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "20%",
-            height: "70%",
-            margin: "30px",
-            marginTop: "140px",
-            marginRight: "200px",
-            borderRadius: "30px",
-            opacity: "0.8",
-            backgroundColor: "white",
-          }}
-        >
-          <form
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h1
-              style={{
-                color: "#E895BC",
-              }}
-            >
-              Sign Up
-            </h1>
-            <input
-              style={{
-                width: "90%",
-                height: "30px",
-                borderRadius: "15px",
-                backgroundColor: "rgba(232, 149, 188, 0.21)",
-                borderWidth: "2px",
-                borderColor: "#E895BC",
-              }}
-              type="text"
-              placeholder="ID"
-              value={Id}
-              onChange={onIdHandler}
-            />
-            <br />
-            <input
-              style={{
-                width: "90%",
-                height: "30px",
-                borderRadius: "15px",
-                backgroundColor: "rgba(232, 149, 188, 0.21)",
-                borderWidth: "2px",
-                borderColor: "#E895BC",
-              }}
-              type="password"
-              placeholder="Password"
-              value={Password}
-              onChange={onPasswordHandler}
-            />
-            <br />
-            <input
-              style={{
-                width: "90%",
-                height: "30px",
-                borderRadius: "15px",
-                backgroundColor: "rgba(232, 149, 188, 0.21)",
-                borderWidth: "2px",
-                borderColor: "#E895BC",
-              }}
-              type="password"
-              placeholder="Confirm Password"
-              value={ConfirmPassword}
-              onChange={onConfirmPasswordHandler}
-            />
-            <br />
-            <input
-              style={{
-                width: "90%",
-                height: "30px",
-                borderRadius: "15px",
-                backgroundColor: "rgba(232, 149, 188, 0.21)",
-                borderWidth: "2px",
-                borderColor: "#E895BC",
-              }}
-              type="email"
-              placeholder="Email Address"
-              value={Email}
-              onChange={onEmailHandler}
-            />
-            <br />
-            <input
-              style={{
-                width: "90%",
-                height: "30px",
-                borderRadius: "15px",
-                backgroundColor: "rgba(232, 149, 188, 0.21)",
-                borderWidth: "2px",
-                borderColor: "#E895BC",
-              }}
-              type="text"
-              placeholder="Full Name"
-              value={Name}
-              onChange={onNameHandler}
-            />
-            <form>
-              <input type="checkbox" />I accept Terms of{" "}
-              <a href="">Use&Privacy Policy</a>
-            </form>
-            <button
-              style={{
-                width: "90%",
-                height: "32px",
-                backgroundColor: "#E895BC",
-                borderRadius: "15px",
-                borderWidth: "0px",
-              }}
-            >
-              Create Account
-            </button>
-          </form>
+    return (
+        <div>
+            <Container>
+                <Box>
+                    <Title>Sign Up</Title>
+                    <Input placeholder="ID"></Input>
+                    <Input placeholder="Password"></Input>
+                    <Input placeholder="Confirm Password"></Input>
+                    <Input placeholder="Email Address"></Input>
+                    <Input placeholder="Phone Number"></Input>
+                    <Input placeholder="Full Name"></Input>
+                    <Content><Checkbox type="checkbox"></Checkbox>I accept The Terms of</Content>
+                    <LineButton>Use&Privacy Policy</LineButton>
+                    <Button>Create Account</Button>
+                </Box>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 }
 
 const Container = styled.div`
@@ -172,6 +32,69 @@ const Container = styled.div`
   height: 100%;
   background-image: url(${back_ground});
   background-size: cover;
+`;
+
+const Box = styled.div`
+  display: flex;
+  float: right;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 25%;
+  height: 80%;
+  margin: 50px;
+  margin-top: 130px;
+  margin-right: 200px;
+  border-radius: 30px;
+  opacity: 0.9;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  color: #e895bc;
+  font-size: 50px;
+`;
+
+const Content = styled.div`
+  color: #e895bc;
+  font-size: 17px;
+`;
+
+const Input = styled.input`
+  width: 70%;
+  height: 30px;
+  background-color: rgba(232, 149, 188, 0.21);
+  border-radius: 15px;
+  border-width: 2px;
+  border-color: #e895bc;
+  margin: 5px;
+`;
+
+const Checkbox = styled.input`
+  width: 20px;
+  height: 20px;
+  margin: 10px;
+`;
+
+const Button = styled.button`
+  width: 70%;
+  height: 50px;
+  background-color: #e895bc;
+  border-radius: 50px;
+  border-width: 0px;
+  margin: 10px;
+  padding: 5px;
+  color: white;
+  font-size: 20px;
+`;
+
+const LineButton = styled.div`
+  margin: 10px;
+  padding: 5px;
+  color: #E870A9;
+  font-size: 17px;
+  border-color: white;
+  text-decoration: underline;
 `;
 
 export default SignupPage;
