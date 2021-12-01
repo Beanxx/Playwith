4f0a7a2a47a92b8f854d044d36c2baf96c78195f
이혜인 <leehi9817@gmail.com>
2021-12-01 15:15:09 +0900

    mysql 연결 test

--- test/tunnel-ssh.js
+++ test/tunnel-ssh.js
@@ -4,9 +4,10 @@ import tunnel from 'tunnel-ssh';
 import dotenv from 'dotenv';
 dotenv.config();
 
+/*
 const ssh_config = {
   username: process.env.SSH_USER,
-  password: process.env.SSH_PASSWORD,
+  //password: process.env.SSH_PASSWORD,
   host: process.env.SSH_HOST,
   port: process.env.SSH_PORT,
   dstHost: process.env.SSH_DATABASE_HOST,
@@ -23,6 +24,34 @@ tunnel(ssh_config, (error, server) => {
       user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
+    })
+    .execute('SHOW TABLES from playwith_db;', (err, result, field) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+});
+*/
+
+const ssh_config = {
+  username: 'ubuntu',
+  //password: process.env.SSH_PASSWORD,
+  host: '15.164.288.184',
+  //port: process.env.SSH_PORT,
+  dstHost: 'playiwth-db-instance.cqtagxkl3688.ap-northeast-2.rds.amazonaws.com',
+  dstPort: 4040,
+}
+
+tunnel(ssh_config, (error, server) => {
+  if (error) {
+    throw error;
+  } else if (server !== null) {
+    mysql
+    .createConnection({
+      host: '0.0.0.0',
+      user: 'withplay',
+      password: 'playwith123',
+      database: 'playwith_db',
     })
     .execute('SHOW TABLES from playwith_db;', (err, result, field) => {
       if (err) throw err;
