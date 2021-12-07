efb5948a1ebde9eba2fd6bea0a1f5771f403a8b3
이혜인 <leehi9817@gmail.com>
2021-12-07 16:52:08 +0900

    Revert "파티룸 탐색 페이지 test"
    This reverts commit c350ee5d2fcf75f989297f67f60ce81896d7b49e.

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,37 +1,10 @@
 //localhost:3001/index/search
 
 const express = require("express");
-const mysql = require("mysql");
-const dbconfig = require("../config/db.js");
-const db = mysql.createConnection(dbconfig);
-const ejs = require('ejs');
-const fs = require('fs');
-const mainPage = fs.readFileSync('../ejs/index.ejs', 'utf8');
+const router = express.Router();
 
-const app = express();
+router.get('/', (req, res) => {
+    res.json({  info: null    });
+})
 
-db.connect((err) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-    console.log("room_list connected.");
-  });
-
-app.get('/', (req, res) => {
-
-    db.query("SELECT * FROM room_list;", function(err, result, fields){
-        if(err) throw err;
-        else {
-            var page = ejs.render(mainPage, {
-                title: "Room List",
-                data: result,
-            });
-            res.send(page);
-        }
-
-    });
-
-});
-
-module.exports = app;
+module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,14 +7,6 @@ const db = mysql.createConnection(dbconfig);
 
 const app = express();
 
-db.connect((err) => {
-  if (err) {
-    console.log(err.message);
-    return;
-  }
-  console.log("users table connected.");
-});
-
 app.get('/', (req, res) => {
     db.query("SELECT * from users", (error, rows) => {
       if (error) throw error;
