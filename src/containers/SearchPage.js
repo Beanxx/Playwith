686411fa621988e4f8839f1c6617986ffbdea370
이혜인 <leehi9817@gmail.com>
2021-12-16 14:51:02 +0900

    SearchPage 수정

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -2,23 +2,10 @@ import React, { useMemo, useEffect, useState } from "react";
 import HomeTemplate from "../components/HomeTemplate";
 import Header from "../components/Header";
 import RoomTable from "../components/RoomTable";
-import faker from "faker/locale/ko";
 import axios from "axios";
 
-faker.seed(100);
-
 function RoomSearch() {
-  const [inputData, setInputData] = useState([
-    {
-      room_id: "",
-      room_title: "",
-      room_subject: "",
-      room_private: "",
-      room_pw: "",
-      room_count: "",
-      room_theme: "",
-    },
-  ]);
+  const [inputData, setInputData] = useState([]);
 
   useEffect(async () => {
     try {
@@ -32,7 +19,9 @@ function RoomSearch() {
         room_count: rowData.room_count,
         room_theme: rowData.room_theme,
       }));
+
       setInputData(inputData.concat(_inputData));
+
     } catch (e) {
       console.error(e.message);
     }
@@ -40,6 +29,10 @@ function RoomSearch() {
 
   const columns = useMemo(
     () => [
+      {
+        accessor: "id",
+        Header: "순번",
+      },
       {
         accessor: "title",
         Header: "제목",
@@ -69,9 +62,11 @@ function RoomSearch() {
       id: rowData.room_id,
       title: rowData.room_title,
       subject: rowData.room_subject,
-      thema: rowData.room_theme,
-      count: rowData.room_count,
-      private: rowData.room_private,
+      thema: (rowData.room_theme == 1 ? "일반" : 
+      (rowData.room_theme == 2 ? "생일" : 
+      (rowData.room_theme == 3 ? "할로윈" : "크리스마스"))),
+      count: rowData.room_count + "/4",
+      private: (rowData.room_private == 0 ? "공개방" : "비밀방"),
     }))
   );
 
